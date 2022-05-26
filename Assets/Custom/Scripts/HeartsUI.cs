2ea5dc0a39e29773f3f55e734d95398f8e8dba3e
Swagguy47 <67041649+Swagguy47@users.noreply.github.com>
2022-05-25 22:02:57 -0500

    First commit of little test idea

--- Assets/Custom/Scripts/HeartsUI.cs
+++ Assets/Custom/Scripts/HeartsUI.cs
@@ -5,11 +5,13 @@ public class HeartsUI : MonoBehaviour
 {
     public PlayerController Player;
     public Transform HeartUI;
+    public Transform ParentPosition;
     
     // God of all jank
     void Update()
     {
-        transform.localPosition = new Vector3(Player.Health * -45, -500, 0);
+        //transform.localPosition = new Vector3(Player.Health * -45, -500, 0);
+        transform.position = ParentPosition.position;
         HeartUI.position = new Vector3(960, 50, 0);
         HeartUI.localScale = new Vector3(Player.Health * 0.3f, 1, 1);
         HeartUI.GetComponent<RawImage>().uvRect = new Rect(0, 0,Player.Health, 1);
